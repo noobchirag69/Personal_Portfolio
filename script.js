0d3282b616e20995d87d0e008bc465c58b55d1fc
noobchirag69 <chiragchakraborty48@gmail.com>
2022-05-16 20:02:00 +0530

    Added 2 projects

--- script.js
+++ script.js
@@ -18,19 +18,19 @@ for (var i = 0; themeDots.length > i; i++) {
 
 function setTheme(mode) {
     if (mode == 'light') {
-        document.getElementById('theme-style').href = 'light.css'
+        document.getElementById('theme-style').href = 'Styles/light.css'
     }
 
     if (mode == 'blue') {
-        document.getElementById('theme-style').href = 'blue.css'
+        document.getElementById('theme-style').href = 'Styles/blue.css'
     }
 
     if (mode == 'green') {
-        document.getElementById('theme-style').href = 'green.css'
+        document.getElementById('theme-style').href = 'Styles/green.css'
     }
     
     if (mode == 'purple') {
-        document.getElementById('theme-style').href = 'purple.css'
+        document.getElementById('theme-style').href = 'Styles/purple.css'
     }
 
     localStorage.setItem('theme', mode)
